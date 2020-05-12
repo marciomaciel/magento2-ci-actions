4d588f4223eec33c134f9f3d88a0a2384cbbbf9d
Marcio Maciel <marcio.maciel@evino.com.br>
2020-05-12 00:23:57 -0300

    Fix tests

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,19 +2,33 @@
 
 set -e
 
-/usr/local/bin/composer install --no-dev --no-progress
+composer install
 chmod +x bin/magento
 
-bin/magento setup:install --admin-firstname="local" --admin-lastname="local" --admin-email="local@local.com" --admin-user="local" --admin-password="local123" --base-url="http://magento.build/" --backend-frontname="admin" --db-host="mysql" --db-name="magento" --db-user="root" --db-password="magento" --use-secure=0 --use-rewrites=1 --use-secure-admin=0 --session-save="db" --currency="EUR" --language="en_US" --timezone="Europe/Rome" --cleanup-database --skip-db-validation
+bin/magento setup:install \
+  --no-interaction \
+  --admin-firstname="local" \
+  --admin-lastname="local" \
+  --admin-email="local@local.com" \
+  --admin-user="local" \
+  --admin-password="local123" \
+  --base-url="http://magento.build/" \
+  --backend-frontname="admin" \
+  --db-host="mysql" \
+  --db-name="magento" \
+  --db-user="root" \
+  --db-password="" \
+  --use-secure=0 \
+  --use-rewrites=1 \
+  --use-secure-admin=0 \
+  --session-save="db" \
+  --currency="BRL" \
+  --language="pt_BR" \
+  --timezone="America/Sao_Paulo" \
+  --cleanup-database
 
-bin/magento setup:di:compile
-bin/magento deploy:mode:set --skip-compilation production
-
-bin/magento setup:static-content:deploy
-#bin/magento setup:static-content:deploy en_US  -a adminhtml
-#bin/magento setup:static-content:deploy fr_FR -f -s standard -a adminhtml
-#bin/magento setup:static-content:deploy fr_FR -f -s standard  -t Creativestyle/theme-creativeshop
-
-composer dump-autoload -o
-
-rm app/etc/env.php
+bin/magento deploy:mode:set production --skip-compilation && \
+bin/magento setup:static-content:deploy --no-interaction --area=frontend -j 4 en_US pt_BR && \
+bin/magento setup:static-content:deploy --no-interaction --area=adminhtml -j 4 en_US pt_BR && \
+bin/magento setup:di:compile --no-interaction && \
+composer dump-autoload --optimize --no-interaction
